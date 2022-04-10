55f1b0fe200d9dfbd6ea21e8ac11b9164f6d1f72
silvia <djmery76@gmail.com>
2022-04-10 17:28:29 +0200

    Aplicacion gifs

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor(private GifsService: GifsService) { }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    if (valor.trim().length === 0) {
+      return;
+    }
+    this.GifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = '';
+  }
+
+}
